e9a5d5a5f1e448a7687d294bb21cce7e9ca548b0
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2025-02-14 22:35:37 +0100

    created basic project structure

---
+++ libs/game/src/lib/game/game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-game',
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.css',
+})
+export class GameComponent {}
