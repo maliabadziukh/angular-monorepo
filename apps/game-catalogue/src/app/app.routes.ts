f64bab66b2f66a397517d059960195cccb12244b
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2025-02-15 17:20:06 +0100

    removed unused libs

--- apps/game-catalogue/src/app/app.routes.ts
+++ apps/game-catalogue/src/app/app.routes.ts
@@ -1,19 +1,5 @@
 import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Route[] = [
-    {
-        path: '',
-        component: NxWelcomeComponent,
-        pathMatch: 'full',
-      },
-      {
-        path: 'game',
-        loadComponent: () =>
-          import('@angular-monorepo/game').then((m) => m.GameComponent),
-      },
-      {
-        path: 'ui',
-        loadComponent: ()=> import('@angular-monorepo/ui').then((m)=>m.UiComponent),
-      }
+    
 ];
