5fb0dcc9c35c05e6d823aca25d829124acb2fae3
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2025-02-15 15:50:44 +0100

    testing router, apps, and libs

--- apps/game-catalogue/src/app/app.routes.ts
+++ apps/game-catalogue/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: NxWelcomeComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'game',
+        loadComponent: () =>
+          import('@angular-monorepo/game').then((m) => m.GameComponent),
+      },
+      {
+        path: 'ui',
+        loadComponent: ()=> import('@angular-monorepo/ui').then((m)=>m.UiComponent),
+      }
+];
--- libs/game/src/lib/game/game.component.ts
+++ libs/game/src/lib/game/game.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiComponent } from '@angular-monorepo/ui';
 
 @Component({
   selector: 'lib-game',
-  imports: [CommonModule],
+  imports: [CommonModule, UiComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css',
 })
