f5ec1b617979aa175fa04a9b04926cebfeaee91a
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2025-02-16 19:22:24 +0100

    hanburger menu open and close with signals

--- libs/shared/ui/organisms/src/lib/menu-modal/menu-modal.component.ts
+++ libs/shared/ui/organisms/src/lib/menu-modal/menu-modal.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@angular-monorepo/molecules';
+import { IconComponent } from '@angular-monorepo/atoms';
 
 @Component({
   selector: 'lib-menu-modal',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent, IconComponent],
   templateUrl: './menu-modal.component.html',
   styleUrl: './menu-modal.component.css',
 })
-export class MenuModalComponent {}
+export class MenuModalComponent {
+  isOpen  = input(false);
+  close = output<void>();
+}
--- libs/shared/ui/organisms/src/lib/navbar/navbar.component.ts
+++ libs/shared/ui/organisms/src/lib/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '@angular-monorepo/atoms';
 import { ButtonComponent } from '@angular-monorepo/molecules';
+import { MenuModalComponent } from '@angular-monorepo/organisms';
 
 @Component({
   selector: 'lib-navbar',
-  imports: [CommonModule, ButtonComponent, IconComponent],
+  imports: [CommonModule, ButtonComponent, IconComponent, MenuModalComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isMenuOpen = signal(false);
+
+  openMenu() {
+    console.log('opening menu')
+    this.isMenuOpen.set(true);
+  }
+
+  closeMenu(){
+    this.isMenuOpen.set(false);
+  }
+}
