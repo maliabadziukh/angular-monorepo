5b0e66c04aa0ae7c9cdd0aaca01df80ef0d8db02
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2025-02-19 19:15:07 +0100

    removed unused dependencies and improved navlink sizing

--- apps/game-catalogue/src/app/pages/game-details/game-details.page.ts
+++ apps/game-catalogue/src/app/pages/game-details/game-details.page.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ButtonComponent}  from "@angular-monorepo/molecules"
-
 @Component({
   selector: 'app-game-details',
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule],
   templateUrl: './game-details.page.html',
   styleUrl: './game-details.page.css',
 })
--- libs/shared/ui/organisms/src/lib/menu-modal/menu-modal.component.ts
+++ libs/shared/ui/organisms/src/lib/menu-modal/menu-modal.component.ts
@@ -1,14 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  ButtonComponent,
-  NavLinksComponent,
-} from '@angular-monorepo/molecules';
+import { ButtonComponent } from '@angular-monorepo/molecules';
 import { IconComponent } from '@angular-monorepo/atoms';
 
 @Component({
   selector: 'lib-menu-modal',
-  imports: [CommonModule, ButtonComponent, IconComponent, NavLinksComponent],
+  imports: [CommonModule, ButtonComponent, IconComponent],
   templateUrl: './menu-modal.component.html',
   styleUrl: './menu-modal.component.css',
 })
